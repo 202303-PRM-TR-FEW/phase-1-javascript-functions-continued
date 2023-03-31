ebdab642b6132663b6a6dc2bb4876378e8ca1a5a
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-04-01 00:26:16 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+  return `This Saturday, I want to ${fun}!`;
+}
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(adj = "*") {
+  let result = function (bla = "special") {
+    return `You are ${adj}${bla}${adj}!`;
+  };
+  return result;
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
